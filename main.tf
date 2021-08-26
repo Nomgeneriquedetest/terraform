ed5e0fba466127ac9cda4601c9a6550e0aa7d3bc
Papibrow <gael.morlin@gmail.com>
2021-08-26 09:02:32 +0200

    modif name

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_ressource_group" "Test_gael" {
+resource "azurerm_resource_group" "Test_gael" {
   name     = "Test_gael"
   location = "westeurope"
 }
